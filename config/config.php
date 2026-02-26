3e03735dd28eb084b7f430af7822bd7fa2cf1e3c
trannhon70 <kevintran351996@gmail.com>
2026-02-26 10:37:46 +0700

    Init full source code

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "phongkhamdakhoanhatviet.vn");
+
+
+// define("DB_HOST", "localhost");
+// define("DB_USER", "sql_benhtri_phongkhamnhatviet_vn");
+// define("DB_PASS", "11f3a6a3396c98");
+// define("DB_NAME", "sql_benhtri_phongkhamnhatviet_vn");
