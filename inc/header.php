868cec5b0acbdb4ed1d3c49f38f0bd6a118c4ecf
trannhon70 <kevintran351996@gmail.com>
2026-02-27 09:48:07 +0700

    update

--- inc/header.php
+++ inc/header.php
@@ -30,8 +30,8 @@ header("Pragma: no-cache");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 header("Cache-Control: max-age=2592000");
 
-$local = 'http://localhost/_nhatvietnew/benhtri.phongkhamnhatviet.vn';
-// $local = 'https://benhtri.phongkhamnhatviet.vn'
+// $local = 'http://localhost/_nhatvietnew/benhtri.phongkhamnhatviet.vn';
+$local = 'https://benhtri.phongkhamnhatviet.vn'
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -39,9 +39,7 @@ $local = 'http://localhost/_nhatvietnew/benhtri.phongkhamnhatviet.vn';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description"
-        content="Phòng khám Nhật Việt chuyên điều trị bệnh nam khoa, bệnh xã hội, da liễu, hậu môn - trực tràng uy tính tại thành phố Hồ Chí Minh">
-    <title>Phòng khám Nhật Việt</title>
+
     <link rel="icon" href="<?php echo $local ?>/images/icons/icon_logo.png" type="image/x-icon">
     <link rel="preload" as="image" href="<?php echo $local ?>/images/banner/mobile_banner.webp" fetchpriority="high"
         media="(max-width: 768px)">
@@ -72,3 +70,31 @@ $local = 'http://localhost/_nhatvietnew/benhtri.phongkhamnhatviet.vn';
         <link rel="stylesheet" href="<?php echo $local ?>/css/header.min.css">
         <link rel="stylesheet" href="<?php echo $local ?>/css/footer.min.css">
     </noscript>
+
+    <script>
+        // Chỉ tải Google Analytics khi người dùng cuộn xuống
+        document.addEventListener('scroll', function loadGA() {
+            console.log('Người dùng cuộn xuống - Tải Google Analytics');
+
+            // Tạo thẻ script
+            var g = document.createElement('script'),
+                s = document.scripts[0];
+            g.src = 'https://www.googletagmanager.com/gtag/js?id=G-WRDVKRY84S';
+            g.async = true;
+            s.parentNode.insertBefore(g, s);
+
+            // Cấu hình gtag
+            g.onload = function() {
+                window.dataLayer = window.dataLayer || [];
+
+                function gtag() {
+                    dataLayer.push(arguments);
+                }
+                gtag('js', new Date());
+                gtag('config', 'G-WRDVKRY84S');
+            };
+
+            // Xóa sự kiện lắng nghe để không tải lại
+            document.removeEventListener('scroll', loadGA);
+        });
+    </script>
